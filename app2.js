e62ceb5e5880a8c1223efe70b713e55891bacd87
Nithin Rokkam <nithinrokkam@gmail.com>
2025-06-16 14:13:09 +0530

    success

--- app2.js
+++ app2.js
@@ -1,5 +1,19 @@
-let juice = 50;
-let gstOnjuice = 0.1;
-let fries = 20;
-let gstOnFries = 0.05;
-console.log("Total amount to be paid is: " + (juice + fries + (juice * gstOnjuice) + (fries * gstOnFries)));
+// let juice = 50;
+// let gstOnjuice = 0.1;
+// let fries = 70;
+// let gstOnFries = 0.05;
+// console.log("Total amount to be paid is: " + (juice + fries + (juice * gstOnjuice) + (fries * gstOnFries)));
+
+console.log("Start");
+
+function calculateJuicebillAmount(){
+    let juice = 50;
+    let gstOnjuice = 0.1;
+    let juiceBillAmount = juice+juice*gstOnjuice;
+    console.log(juiceBillAmount);
+}
+console.log("Mid");
+
+calculateJuicebillAmount();
+
+console.log("End");
