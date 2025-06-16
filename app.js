f06f6055af66c59043a9f78d8cc3184e6f5d467f
Nithin Rokkam <nithinrokkam@gmail.com>
2025-06-16 13:19:10 +0530

    success

---
+++ app.js
@@ -0,0 +1,16 @@
+window.console.log("Hello from JS!");
+// let, const, var
+// let age = 20;
+//const age = 20;
+//var age = 20;
+// console.log(age);
+let age = 20;
+console.log(age);
+age=23;
+console.log(age);
+console.log("a"*1);
+// console.log(age);
+// var age=21;
+// console.log(age);
+// var age=22;
+// console.log(age);
