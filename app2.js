daff0a6405103040bfaf5467db0df8e8f914f459
Nithin Rokkam <nithinrokkam@gmail.com>
2025-06-16 13:58:28 +0530

    success

---
+++ app2.js
@@ -0,0 +1,5 @@
+let juice = 50;
+let gstOnjuice = 0.1;
+let fries = 20;
+let gstOnFries = 0.05;
+console.log("Total amount to be paid is: " + (juice + fries + (juice * gstOnjuice) + (fries * gstOnFries)));
