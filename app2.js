222fb71fcd613122d0b8abf8a27954ace156e68b
Nithin Rokkam <nithinrokkam@gmail.com>
2025-06-16 14:45:22 +0530

    success

--- app2.js
+++ app2.js
@@ -6,14 +6,25 @@
 let age = 22;
 console.log("Start", age);
 
-function calculateJuicebillAmount(){
-    let juice = 50;
-    let gstOnjuice = 0.1;
-    let juiceBillAmount = juice+juice*gstOnjuice;
-    console.log(juiceBillAmount);
+function printBill(name, amount){
+    console.log("--------------");
+    console.log("Name: " + name);
+    console.log("Amount: " + amount);
+    console.log("--------------");
+} 
+
+function calculatebillAmount(price,gst){
+    // let juice = 50;
+    // let gstOnjuice = 0.1;
+    console.log("calculatebillAmount....");
+    let BillAmount = price+price*gst;
+    // console.log(juiceBillAmount);
+    return BillAmount;
 }
 console.log("Mid");
 
-calculateJuicebillAmount();
-
+const res1 = calculatebillAmount(50,0.1);
+printBill("Juice", res1);
+const res2 = calculatebillAmount(60,0.2);
+printBill("Fries", res2);
 console.log("End");
