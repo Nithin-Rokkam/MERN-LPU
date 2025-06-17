4dccaae4fa75746547f8c16c98fc84b53878bf00
Nithin Rokkam <nithinrokkam@gmail.com>
2025-06-17 10:01:04 +0530

    success

--- app2.js
+++ app2.js
@@ -3,31 +3,31 @@
 // let fries = 70;
 // let gstOnFries = 0.05;
 // console.log("Total amount to be paid is: " + (juice + fries + (juice * gstOnjuice) + (fries * gstOnFries)));
-let age = 22;
-console.log("Start", age);
+// let age = 22;
+// console.log("Start", age);
 
-function printBill(name, amount){
-    console.log("--------------");
-    console.log("Name: " + name);
-    console.log("Amount: " + amount);
-    console.log("--------------");
-} 
+// function printBill(name, amount){
+//     console.log("--------------");
+//     console.log("Name: " + name);
+//     console.log("Amount: " + amount);
+//     console.log("--------------");
+// } 
 
-function calculatebillAmount(price, gst){
-    console.log("calculatebillAmount....");
-    let BillAmount = price + price * gst;
-    return BillAmount;
-}
+// function calculatebillAmount(price, gst){
+//     console.log("calculatebillAmount....");
+//     let BillAmount = price + price * gst;
+//     return BillAmount;
+// }
 
-console.log("Mid");
+// console.log("Mid");
 
-const res1 = calculatebillAmount(50, 0.1);
-printBill("Juice", res1);
+// const res1 = calculatebillAmount(50, 0.1);
+// printBill("Juice", res1);
 
-const res2 = calculatebillAmount(60, 0.2);
-printBill("Fries", res2);
+// const res2 = calculatebillAmount(60, 0.2);
+// printBill("Fries", res2);
 
-console.log("End");
+// console.log("End");
 
 // Arrow function version  DAY2
 const FinalBill = (label, amount) => {
