e68347f63d9ac1503c852da395406e2cff2aa1d4
Nithin Rokkam <nithinrokkam@gmail.com>
2025-06-17 11:53:28 +0530

    success

--- app2.js
+++ app2.js
@@ -30,74 +30,77 @@
 // console.log("End");
 
 // Arrow function version  DAY2
-const FinalBill = (label, amount) => {
-    console.log("--------------");
-    console.log(label + ": " + amount);
-    console.log("--------------");
-};
+// const FinalBill = (label, amount) => {
+//     console.log("--------------");
+//     console.log(label + ": " + amount);
+//     console.log("--------------");
+// };
 
-const calculateBillAmountForFood = (price) => price * 1.05;
-const calculateBillAmountForClothes = (price) => price * 1.12;
-const calculateBillAmountForDrinks = (price) => price * 1.12;
+// const calculateBillAmountForFood = (price) => price * 1.05;
+// const calculateBillAmountForClothes = (price) => price * 1.12;
+// const calculateBillAmountForDrinks = (price) => price * 1.12;
 
-const calculateBillForFoodandDrinks = (txt) => {
-    console.log("Calculating bill for Food and drinks: " + txt);
-    return txt;
-};
-const calculateBillForFood = (txt) => {
-    console.log("Calculating bill for Food : " + txt);
-    return txt;
-};
-const calculateBillForAll = (txt) => {
-    console.log("Calculating bill for Food drinks and clothes : " + txt);
-    return txt;
-};
+// const calculateBillForFoodandDrinks = (txt) => {
+//     console.log("Calculating bill for Food and drinks: " + txt);
+//     return txt;
+// };
+// const calculateBillForFood = (txt) => {
+//     console.log("Calculating bill for Food : " + txt);
+//     return txt;
+// };
+// const calculateBillForAll = (txt) => {
+//     console.log("Calculating bill for Food drinks and clothes : " + txt);
+//     return txt;
+// };
 
-const finalBillAmount = (food, clothes, drinks,cb) => {
-    const foodBill = calculateBillAmountForFood(food);
-    const clothesBill = calculateBillAmountForClothes(clothes);
-    const drinksBill = calculateBillAmountForDrinks(drinks);
+// const finalBillAmount = (food, clothes, drinks,cb) => {
+//     const foodBill = calculateBillAmountForFood(food);
+//     const clothesBill = calculateBillAmountForClothes(clothes);
+//     const drinksBill = calculateBillAmountForDrinks(drinks);
     
-    const totalBill = foodBill + clothesBill + drinksBill;
+//     const totalBill = foodBill + clothesBill + drinksBill;
     
-    cb(totalBill);
-};
+//     cb(totalBill);
+// };
 
-finalBillAmount(100, 200, 300, calculateBillForAll);
-finalBillAmount(100, 200, 0, calculateBillForFoodandDrinks);
-finalBillAmount(100, 0, 0, calculateBillForFood);
+// finalBillAmount(100, 200, 300, calculateBillForAll);
+// finalBillAmount(100, 200, 0, calculateBillForFoodandDrinks);
+// finalBillAmount(100, 0, 0, calculateBillForFood);
 
 // objects
-const cse7001 ={
-    name: "XYZ",
-    address: "123 Main St",
-    bloodGroup: "O+"
-};
-console.log(cse7001);
+// const cse7001 ={
+//     name: "XYZ",
+//     address: "123 Main St",
+//     bloodGroup: "O+"
+// };
+// console.log(cse7001);
 
-const cse7002 = {
-    name: "ABC",
-    address: "456 Elm St",
-    bloodGroup: "A-"
-};
-console.log(cse7002);
-// READ the value
-const StudentName = cse7001.name;
-console.log("Student Name: " + StudentName);
-// Update the value
-cse7001.name = "PQR";
-console.log("Updated Student Name: " + cse7001.name);
-// Add a new property
-cse7001.age = 22;
-console.log("Added Age: " + cse7001.age);
-console.log("Updated Object: ", cse7001);
-// Delete a property
-delete cse7001.bloodGroup;
-console.log("After Deletion: ", cse7001);
+// const cse7002 = {
+//     name: "ABC",
+//     address: "456 Elm St",
+//     bloodGroup: "A-"
+// };
+// console.log(cse7002);
+// // READ the value
+// const StudentName = cse7001.name;
+// console.log("Student Name: " + StudentName);
+// // Update the value
+// cse7001.name = "PQR";
+// console.log("Updated Student Name: " + cse7001.name);
+// // Add a new property
+// cse7001.age = 22;
+// console.log("Added Age: " + cse7001.age);
+// console.log("Updated Object: ", cse7001);
+// // Delete a property
+// delete cse7001.bloodGroup;
+// console.log("After Deletion: ", cse7001);
 
-delete cse7001.city;
-console.log("After Deletion of city: ", cse7001);
+// delete cse7001.city;
+// console.log("After Deletion of city: ", cse7001);
 
+
+//this keyword
+// The 'this' keyword refers to the current object in the context of a method.
 const person ={
     name:'nithin',
     height: 165,
@@ -107,14 +110,55 @@ const person ={
     getBMI: function (){
         console.log(this.height, this.weight);
         console.log("Calculating BMI...");
+        let bmi;
         if(this.height && this.weight){
             const heightInMeters = this.height / 100;
-            const bmi = this.weight / (heightInMeters * heightInMeters);
-            console.log("BMI is: " + bmi.toFixed(2));
+            bmi = this.weight / (heightInMeters ** 2);
+            console.log(`BMI for ${this.name} is ${bmi}`);
         }else{
             console.log("Height or weight is not defined.");
+        }
+        if(bmi<20){
+            console.log("It seems he is underweight.");
+        }else if(bmi>=20 && bmi<25){
+            console.log("It seems he is healthy.");
+        }else if(bmi>=25 && bmi<30){
+            console.log("It seems he is overweight.");
+        }else{
+            console.log("It seems he is obese.");
         }
     }
 };
 
 person.getBMI();
+person.weight = 60;
+person.getBMI();
+
+// Dynamic value access
+
+
+//mutation
+const person1={
+    name: 'nithin',
+    height: 165,
+    weight: 64,
+    college: 'LPU',
+    rollNumber: '7001'
+};
+
+const person2 = person1; // This is a reference, not a copy
+console.log("Before mutation: ", person1, person2);
+console.log("Are they same? ", person1 === person2); //return boolean
+person1.name="raj";
+person2.name="rajkumar";
+console.log("After mutation: ", person1, person2);
+console.log("Are they same? ", person1 === person2); //return boolean
+
+
+//de-structuring
+const {weight, college} = person1;
+console.log("Weight: ", weight);
+console.log("College: ", college);
+
+//spread operator removes the reference i.e the outer layer of the object
+const person3 = {...person1}; // This creates a shallow copy of person1
