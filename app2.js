afb3d105f92cbcdc04b8bf04587b106d895db02b
Nithin Rokkam <nithinrokkam@gmail.com>
2025-06-17 09:31:01 +0530

    success

--- app2.js
+++ app2.js
@@ -13,18 +13,41 @@ function printBill(name, amount){
     console.log("--------------");
 } 
 
-function calculatebillAmount(price,gst){
-    // let juice = 50;
-    // let gstOnjuice = 0.1;
+function calculatebillAmount(price, gst){
     console.log("calculatebillAmount....");
-    let BillAmount = price+price*gst;
-    // console.log(juiceBillAmount);
+    let BillAmount = price + price * gst;
     return BillAmount;
 }
+
 console.log("Mid");
 
-const res1 = calculatebillAmount(50,0.1);
+const res1 = calculatebillAmount(50, 0.1);
 printBill("Juice", res1);
-const res2 = calculatebillAmount(60,0.2);
+
+const res2 = calculatebillAmount(60, 0.2);
 printBill("Fries", res2);
+
 console.log("End");
+
+// Arrow function version
+const FinalBill = (label, amount) => {
+    console.log("--------------");
+    console.log(label + ": " + amount);
+    console.log("--------------");
+};
+
+const calculateBillAmountForFood = (price) => price * 1.05;
+const calculateBillAmountForClothes = (price) => price * 1.12;
+const calculateBillAmountForDrinks = (price) => price * 1.12;
+
+const finalBillAmount = (food, clothes, drinks) => {
+    const foodBill = calculateBillAmountForFood(food);
+    const clothesBill = calculateBillAmountForClothes(clothes);
+    const drinksBill = calculateBillAmountForDrinks(drinks);
+    
+    const totalBill = foodBill + clothesBill + drinksBill;
+    
+    FinalBill("Total Bill", totalBill);
+};
+
+finalBillAmount(100, 200, 300);
