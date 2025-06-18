ec3f79d0bd719822be884cdd456337b25877729a
Nithin Rokkam <nithinrokkam@gmail.com>
2025-06-18 11:50:08 +0530

    done

---
+++ Bom&Dom.js
@@ -0,0 +1,18 @@
+// console.log("window object", window); // Logs the global window object in browsers
+// console.log("document object", window.document); // Logs the document object representing the HTML document
+
+// const headings = document.getElementsByTagName("h1");
+// // console.log("Headings:", headings); // Logs all <h1> elements in the document
+
+// headings[0].innerText = "Updated Heading"; // Updates the text of the first <h1> element
+// // headings[1].innerText = "Another Heading"; // Updates the text of the second <h1> element
+// console.log("Updated Headings:", headings); // Logs the updated <h1> elements
+
+// const element = document.getElementsByClassName("text-brown");
+// console.log("Headings with class 'text-brown':", element); // Logs all elements with class 'text-brown
+
+// element[0].computedStyleMap.color = "brown";
+// element[1].style.color = "brown"; // Changes the text color of the first element with class 'text-brown'
+
+const text = document.getElementById("text1");
+text.style.backgroundColor = "green";
